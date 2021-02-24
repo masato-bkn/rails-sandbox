b37a8aad45d6919e171c473372a6ec26518f672e
重田 将人 <masato.0928.j@gmail.com>
2021-02-24 18:29:22 +0900

    i18のセットアップをして、GET comics/:idがnot_foundの場合にja.ymlに定義したエラーメッセージを返すようにした

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module Sandbox
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = :ja
   end
 end
