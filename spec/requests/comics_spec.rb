eb6d10f188a25a0f022d74ec71e5c46c986a5401
重田 将人 <masato.0928.j@gmail.com>
2021-03-04 21:02:51 +0900

    showについての区分けを作った

--- spec/requests/comics_spec.rb
+++ spec/requests/comics_spec.rb
@@ -3,35 +3,37 @@
 require 'rails_helper'
 
 RSpec.describe ComicsController, type: :request do
-  subject do
-    get comic_path(id: comic_id)
-    response
-  end
+  describe 'showについて' do
+    subject do
+      get comic_path(id: comic_id)
+      response
+    end
 
-  let :comic do
-    create(:comic)
-  end
+    let :comic do
+      create(:comic)
+    end
 
-  let :comic_id do
-    comic.id
-  end
-
-  it 'returns success' do
-    expect(subject).to have_http_status(:success)
-  end
-
-  context '指定したidに紐付くcomicが存在しない場合' do
     let :comic_id do
-      999
+      comic.id
     end
 
-    it 'returns not_found' do
-      expect(subject).to have_http_status(:not_found)
+    it 'returns success' do
+      expect(subject).to have_http_status(:success)
     end
 
-    it ' 想定したメッセージが返却されること' do
-      result = JSON.parse(subject.body)
-      expect(result['message']).to eq('存在するcomic_idを指定してください')
+    context '指定したidに紐付くcomicが存在しない場合' do
+      let :comic_id do
+        999
+      end
+
+      it 'returns not_found' do
+        expect(subject).to have_http_status(:not_found)
+      end
+
+      it ' 想定したメッセージが返却されること' do
+        result = JSON.parse(subject.body)
+        expect(result['message']).to eq('存在するcomic_idを指定してください')
+      end
     end
   end
 end
